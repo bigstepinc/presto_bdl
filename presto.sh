fdabe34ee4706346993128673041a3c5c998bf90
cristina-grosu <cristina.grosu@bigstep.com>
2019-05-31 12:33:45 +0300

    Update presto.sh

--- presto.sh
+++ presto.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+./etc/presto/init.sh
+
 cp /etc/presto/*.properties $PRESTO_CONF_DIR
 cp /etc/presto/*.config $PRESTO_CONF_DIR
 cp /etc/presto/catalog/*properties $PRESTO_CONF_DIR/catalog
