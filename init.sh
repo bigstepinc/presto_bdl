a8e9599cd014713a667eb1eb7c4c6c9d722b5801
cristina-grosu <cristina.grosu@bigstep.com>
2019-05-31 11:43:29 +0300

    Create init.sh

---
+++ init.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+# define the config.properties file
+touch /presto/etc/config.properties
+if [ "$MODE" == "coordinator" ]; then
+	echo "coordinator=true" >> /presto/etc/config.properties
+  echo "node-scheduler.include-coordinator=false"  >> /presto/etc/config.properties
+  echo "http-server.http.port=8080" >> /presto/etc/config.properties
+  echo "query.max-memory=4GB" >> /presto/etc/config.properties
+  echo "query.max-memory-per-node=1GB" >> /presto/etc/config.properties
+  echo "discovery-server.enabled=true" >> /presto/etc/config.properties
+else 
+  echo "coordinator=false" >> /presto/etc/config.properties
+  echo "http-server.http.port=8080" >> /presto/etc/config.properties
+  echo "query.max-memory=4GB" >> /presto/etc/config.properties
+  echo "query.max-memory-per-node=1GB" >> /presto/etc/config.properties
+fi
+
+if [ "$DISCOVERY_URI" != "" ]; then
+      echo "discovery.uri=http://$DISCOVERY_URI:8080"  >> /presto/etc/config.properties
+fi
+
+cp /presto/etc/config.properties /etc/presto/config.properties
+
+
+
