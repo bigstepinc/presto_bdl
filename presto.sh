7d8095296e56389a85205578d367d08ade3030db
cristina-grosu <cristina.grosu@bigstep.com>
2019-08-02 17:30:37 +0300

    Update presto.sh

--- presto.sh
+++ presto.sh
@@ -1,8 +1,18 @@
 #!/bin/bash
 
-ls /etc/presto/catalog
-sleep 100
-ls /etc/presto/catalog
-cp --verbose /etc/presto/*.properties /presto/etc
-cp --verbose /etc/presto/*.config /presto/etc
-cp --verbose /etc/presto/catalog/*properties /presto/etc/catalog
+#ls /etc/presto/catalog
+
+ln -s /etc/presto /presto/etc
+#ln -s /etc/presto/log.properties /presto/etc/log.properties
+#ln -s /etc/presto/node.properties /presto/etc/node.properties
+
+#ln -s /etc/presto/jvm.config /presto/etc/jvm.config
+
+#ln -s /etc/presto/catalog/log.properties /presto/etc/catalog/log.properties
+#ln -s /etc/presto/catalog/log.properties /presto/etc/catalog/log.properties
+
+#ls /etc/presto/catalog
+
+#cp --verbose /etc/presto/*.properties /presto/etc
+#cp --verbose /etc/presto/*.config /presto/etc
+#cp --verbose /etc/presto/catalog/*properties /presto/etc/catalog
