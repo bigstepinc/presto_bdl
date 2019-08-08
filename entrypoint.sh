14aa3eeddd7fad232d96b41b7a1381fe9489c121
cristina-grosu <cristina.grosu@bigstep.com>
2019-08-08 18:21:13 +0300

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -8,6 +8,13 @@ rm /presto/plugin/hive-hadoop2/jackson-core-2.9.8.jar
 rm /presto/plugin/hive-hadoop2/jackson-databind-2.9.8.jar	
 rm /presto/plugin/hive-hadoop2/jackson-datatype-joda-2.9.8.jar
 
+cp /opt/bigstepdatalake-0.12.3/lib/aws-java-sdk-1.7.4.jar /presto/etc/data/plugin/hive-hadoop2/
+rm /presto/etc/data/plugin/hive-hadoop2/gcs-connector-hadoop2-1.9.10.jar
+rm /presto/etc/data/plugin/hive-hadoop2/gcsio-1.9.10.jar
+rm /presto/etc/data/plugin/hive-hadoop2/jackson-core-2.9.8.jar
+rm /presto/etc/data/plugin/hive-hadoop2/jackson-databind-2.9.8.jar	
+rm /presto/etc/data/plugin/hive-hadoop2/jackson-datatype-joda-2.9.8.jar
+
 ./tmp/docker-presto.sh
 
 launcher run
