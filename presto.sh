aa0438c21f9ffed5c20fd0183a8f37051b4455f8
cristina-grosu <cristina.grosu@bigstep.com>
2019-08-22 13:21:52 +0300

    Update presto.sh

--- presto.sh
+++ presto.sh
@@ -3,3 +3,5 @@
 cp --verbose /etc/presto/*.properties /presto/etc
 cp --verbose /etc/presto/*.config /presto/etc
 cp --verbose /etc/presto/catalog/*properties /presto/etc/catalog
+
+rm /presto/plugin/hive-hadoop2/jackson-databind-2.8.9.jar
